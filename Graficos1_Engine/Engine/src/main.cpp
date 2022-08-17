2010a8d2364814dc13d1ffa7d40297f2af66f826
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-08-17 18:30:02 -0300

    Update main

--- Graficos1_Engine/Engine/src/main.cpp
+++ Graficos1_Engine/Engine/src/main.cpp
@@ -3,7 +3,15 @@
 int main()
 {
 	Base* base = new Base();
-	base->DoFunnyStuff();
+	while (base->IsRunning())
+	{
+		base->Loop();
+	}
 	delete base;
 	return 0;
+}
+
+void Base::Draw()
+{
+ //do funny stuff
 }
