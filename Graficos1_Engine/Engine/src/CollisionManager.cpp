9fb9784002c896a79c8820aa16b572dade73749e
Ale Insausti <alejandroninsausti@gmail.com>
2022-11-30 14:25:51 -0300

    Add CollisionManager rework (WIP)
    The rework is not working, but it's commented. The old version is still uncommented and working

--- Graficos1_Engine/Engine/src/CollisionManager.cpp
+++ Graficos1_Engine/Engine/src/CollisionManager.cpp
@@ -34,24 +34,43 @@ bool CollisionManager::CheckCollision(Entity2D* entity1, Entity2D* entity2)
 	//return x1 < x2 + width2 && x1 + width1 > x2 && y1 < y2 + height2 && y1 + height1 > y2;
 }
 
-bool CollisionManager::CheckCollision(Sprite* entity1, Sprite* entity2)
+bool CollisionManager::CheckCollision(Sprite* sprite1, Sprite* sprite2)
 {
-	int x1 = entity1->GetTranslation().x;
-	int y1 = entity1->GetTranslation().y;
-	int width1 = entity1->GetWidth() / 2 + entity1->GetScale().x / 2;
-	int height1 = entity1->GetHeight() / 2 + entity1->GetScale().y / 2;
+	int x1 = sprite1->GetTranslation().x;
+	int y1 = sprite1->GetTranslation().y;
+	int width1 = sprite1->GetWidth() / 2 + sprite1->GetScale().x / 2;
+	int height1 = sprite1->GetHeight() / 2 + sprite1->GetScale().y / 2;
 
-	int x2 = entity2->GetTranslation().x;
-	int y2 = entity2->GetTranslation().y;
-	int width2 = entity2->GetWidth() / 2 + entity2->GetScale().x / 2;
-	int height2 = entity2->GetHeight() / 2 + entity2->GetScale().y / 2;
+	int x2 = sprite2->GetTranslation().x;
+	int y2 = sprite2->GetTranslation().y;
+	int width2 = sprite2->GetWidth() / 2 + sprite2->GetScale().x / 2;
+	int height2 = sprite2->GetHeight() / 2 + sprite2->GetScale().y / 2;
+
+	//int x1 = sprite1->GetTranslation().x;
+	//int y1 = sprite1->GetTranslation().y;
+	//int width1 = ((sprite1->GetWidth() / 6) * sprite1->GetScale().x) / 2;
+	//int height1 = (sprite1->GetHeight() * sprite1->GetScale().y) / 2;
+
+	//int x2 = sprite2->GetTranslation().x;
+	//int y2 = sprite2->GetTranslation().y;
+	//int width2 = ((sprite2->GetWidth() / 6) * sprite2->GetScale().x) / 2;
+	//int height2 = (sprite2->GetHeight() / sprite2->GetScale().y) / 2;
 
 	bool right1Collision = x1 < x2 + width2;
 	bool left1Collision = x1 + width1 > x2;
 	bool top1Collision = y1 < y2 + height2;
 	bool bot1Collision = y1 + height1 > y2;
 
+	//CLAVE
+	//bool right1Collision = x1 + width1 > x2 - width2;
+	//bool left1Collision = x1 - width1 < x2 + width2;
+	//bool top1Collision = y1 + height1 > y2 - height2;
+	//bool bot1Collision = y1 - height1 < y2 + height2;
+
+	//CLAVE
 	bool collision = right1Collision && left1Collision && top1Collision && bot1Collision;
+
+	//bool collision = (right1Collision || left1Collision) && (top1Collision || bot1Collision);
 	//bool collision = x1 < x2 + width2 && x1 + width1 > x2 && y1 < y2 + height2 && y1 + height1 > y2;
 
 	return collision;
