8af969e3ed4c1227b5c37bd4c47f354a544f8658
Alejandro Insausti <alejandroninsausti@gmail.com>
2022-12-06 12:37:44 -0300

    Fix Square Collision
    Halving by 2 scale of every entity fixed the issue

--- Graficos1_Engine/Engine/src/CollisionManager.cpp
+++ Graficos1_Engine/Engine/src/CollisionManager.cpp
@@ -16,38 +16,20 @@ bool CollisionManager::CheckCollision(Entity2D* entity1, Entity2D* entity2)
 	int x2 = entity2->GetTranslation().x;
 	int y2 = entity2->GetTranslation().y;
 
-	//int width1 = entity1->GetScale().x;
-	//int height1 = entity1->GetScale().y;
-
-	//int width2 = entity2->GetScale().x;
-	//int height2 = entity2->GetScale().y;
-
 	int xHalf1 = entity1->GetScale().x / 2;
 	int yHalf1 = entity1->GetScale().y / 2;
 
 	int xHalf2 = entity2->GetScale().x / 2;
 	int yHalf2 = entity2->GetScale().y / 2;
 
-	//bool right1Collision = x1 < x2 + width2;
-	//bool left1Collision = x1 + width1 > x2;
-	//bool top1Collision = y1 < y2 + height2;
-	//bool bot1Collision = y1 + height1 > y2;
-
-	//CLAVE
 	bool right1Collision = x1 + xHalf1 > x2 - xHalf2;
 	bool left1Collision = x1 - xHalf1 < x2 + xHalf2;
 	bool top1Collision = y1 + yHalf1 > y2 - yHalf2;
 	bool bot1Collision = y1 - yHalf1 < y2 + yHalf2;
 
-	//CLAVE
 	bool collision = right1Collision && left1Collision && top1Collision && bot1Collision;
 
-	//bool collision = (right1Collision || left1Collision) && (top1Collision || bot1Collision);
-	//bool collision = x1 < x2 + width2 && x1 + width1 > x2 && y1 < y2 + height2 && y1 + height1 > y2;
-
 	return collision;
-
-	//return x1 < x2 + width2 && x1 + width1 > x2 && y1 < y2 + height2 && y1 + height1 > y2;
 }
 
 bool CollisionManager::CheckCollision(Sprite* sprite1, Sprite* sprite2)
@@ -63,11 +45,17 @@ bool CollisionManager::CheckCollision(Sprite* sprite1, Sprite* sprite2)
 	//int width2 = sprite2->GetWidth() + sprite2->GetScale().x;
 	//int height2 = sprite2->GetHeight() + sprite2->GetScale().y;
 
-	int width1 = sprite1->GetWidth() / 2 + sprite1->GetScale().x / 2;
-	int height1 = sprite1->GetHeight() / 2 + sprite1->GetScale().y / 2;
+	//int width1 = sprite1->GetWidth() / 2 + sprite1->GetScale().x / 2;
+	//int height1 = sprite1->GetHeight() / 2 + sprite1->GetScale().y / 2;
 
-	int width2 = sprite2->GetWidth() / 2 + sprite2->GetScale().x / 2;
-	int height2 = sprite2->GetHeight() / 2 + sprite2->GetScale().y / 2;
+	//int width2 = sprite2->GetWidth() / 2 + sprite2->GetScale().x / 2;
+	//int height2 = sprite2->GetHeight() / 2 + sprite2->GetScale().y / 2;
+
+	int width1 = sprite1->GetWidth() / sprite1->GetSpriteQty();
+	int height1 = sprite1->GetHeight();
+
+	int width2 = sprite2->GetWidth() / sprite2->GetSpriteQty();
+	int height2 = sprite2->GetHeight();
 
 	//int width1 = ((sprite1->GetWidth() / 6) * sprite1->GetScale().x) / 2;
 	//int height1 = (sprite1->GetHeight() * sprite1->GetScale().y) / 2;
@@ -75,16 +63,16 @@ bool CollisionManager::CheckCollision(Sprite* sprite1, Sprite* sprite2)
 	//int width2 = ((sprite2->GetWidth() / 6) * sprite2->GetScale().x) / 2;
 	//int height2 = (sprite2->GetHeight() / sprite2->GetScale().y) / 2;
 
-	bool right1Collision = x1 < x2 + width2;
-	bool left1Collision = x1 + width1 > x2;
-	bool top1Collision = y1 < y2 + height2;
-	bool bot1Collision = y1 + height1 > y2;
+	//bool right1Collision = x1 < x2 + width2;
+	//bool left1Collision = x1 + width1 > x2;
+	//bool top1Collision = y1 < y2 + height2;
+	//bool bot1Collision = y1 + height1 > y2;
 
 	//CLAVE
-	//bool right1Collision = x1 + width1 > x2 - width2;
-	//bool left1Collision = x1 - width1 < x2 + width2;
-	//bool top1Collision = y1 + height1 > y2 - height2;
-	//bool bot1Collision = y1 - height1 < y2 + height2;
+	bool right1Collision = x1 + width1 > x2 - width2;
+	bool left1Collision = x1 - width1 < x2 + width2;
+	bool top1Collision = y1 + height1 > y2 - height2;
+	bool bot1Collision = y1 - height1 < y2 + height2;
 
 	//CLAVE
 	bool collision = right1Collision && left1Collision && top1Collision && bot1Collision;
--- Graficos1_Engine/Engine/src/Entity/Entity.cpp
+++ Graficos1_Engine/Engine/src/Entity/Entity.cpp
@@ -93,7 +93,7 @@ void Entity::UpdateModel(bool isModelCreated)
 {
 	glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(translation.x, translation.y, 0.0f));
 	glm::mat4 rot = glm::rotate(glm::mat4(1.0f), (rotation * 3.14f) / 180, glm::vec3(0, 0, 1)); // where x, y, z is axis of rotation (e.g. 0 1 0)
-	glm::mat4 scal = glm::scale(glm::mat4(1.0f), glm::vec3(scale.x, scale.y, 0.0f));
+	glm::mat4 scal = glm::scale(glm::mat4(1.0f), glm::vec3(scale.x / 2, scale.y / 2, 0.0f));
 
 	glm::mat4 model = trans * rot * scal;
 
